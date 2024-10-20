8f2c17353faced38f23652bca6ff8485fbb1bd0c
irinakalman <kalman0292@gmail.com>
2024-10-20 20:14:35 +0300

    added routing

--- pageKeeper/src/app/app.routes.ts
+++ pageKeeper/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books.component';
+import { CustomersComponent } from './customers/customers.component';
+import { ReservationsComponent } from './reservations/reservations.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'books', component: BooksComponent},
+  {path: 'customers', component: CustomersComponent},
+  {path: 'reservations', component: ReservationsComponent}
+];
