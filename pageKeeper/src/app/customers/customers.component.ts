198116d4c06245e7e658dd3caf64e58c89f2367f
georgebsr <georgebsr@outlook.com>
2024-10-23 19:20:24 +0300

    Dummy buttons added on customers

--- pageKeeper/src/app/customers/customers.component.ts
+++ pageKeeper/src/app/customers/customers.component.ts
@@ -13,10 +13,24 @@ import { provideHttpClient } from '@angular/common/http';
 })
 export class CustomersComponent implements OnInit {
   customers: any[] = [];
+
   constructor(private customerService: CustomerService) {}
+
   ngOnInit(): void {
     this.customerService.getCustomers().subscribe((data: any) => {
       this.customers = data;
     });
+  }
+
+  viewCustomer(customer: any) {
+    console.log('Viewing customer:', customer);
+  }
+
+  editCustomer(customer: any) {
+    console.log('Editing customer:', customer);
+  }
+
+  deleteCustomer(customer: any) {
+    console.log('Deleting customer:', customer);
   }
 }
