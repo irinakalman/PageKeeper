c02a08d62da0c745efe0a0e90831bb24709500a2
georgebsr <georgebsr@outlook.com>
2024-10-23 19:01:52 +0300

    Home and Customers

--- pageKeeper/src/app/customers/customers.component.ts
+++ pageKeeper/src/app/customers/customers.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { CustomerService } from '../services/customer.service';
+import { CommonModule } from '@angular/common';
+import { provideHttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-customers',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './customers.component.html',
-  styleUrl: './customers.component.scss'
+  styleUrl: './customers.component.scss',
 })
-export class CustomersComponent {
-
+export class CustomersComponent implements OnInit {
+  customers: any[] = [];
+  constructor(private customerService: CustomerService) {}
+  ngOnInit(): void {
+    this.customerService.getCustomers().subscribe((data: any) => {
+      this.customers = data;
+    });
+  }
 }
--- pageKeeper/src/app/home/home.component.ts
+++ pageKeeper/src/app/home/home.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { BookService } from '../services/books.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  availableBooks: any[] = [];
 
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    this.bookService.getBooks().subscribe((books: any) => {
+      this.availableBooks = books.filter(
+        (book: any) => book.available === true
+      );
+    });
+  }
 }
