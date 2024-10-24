214fcbfbd9fbf47155e206dc5b3cfcb458ff78b5
irinakalman <kalman0292@gmail.com>
2024-10-25 00:57:03 +0300

    header css, added sidebar

--- pageKeeper/src/app/app.component.ts
+++ pageKeeper/src/app/app.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { HeaderComponent } from './header/header.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, RouterLink, RouterLinkActive, HeaderComponent],
+  imports: [RouterOutlet, HomeComponent, RouterLink, RouterLinkActive, HeaderComponent, MatSidenavModule, MatListModule, MatFormFieldModule, MatInputModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- pageKeeper/src/app/header/header.component.ts
+++ pageKeeper/src/app/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, RouterModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, RouterModule, MatToolbarModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
