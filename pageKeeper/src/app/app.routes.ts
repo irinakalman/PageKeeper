3961288f19bd59e94f594d8bb004f643fedd2204
irinakalman <81618370+irinakalman@users.noreply.github.com>
2024-11-03 01:40:20 +0200

    reservations

--- pageKeeper/src/app/app.routes.ts
+++ pageKeeper/src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { HomeComponent } from './home/home.component';
 import { BooksComponent } from './books/books.component';
 import { CustomersComponent } from './customers/customers.component';
 import { ReservationsComponent } from './reservations/reservations.component';
+import { ReservationFormComponent } from './reservations/reservation-form/reservation-form.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'books', component: BooksComponent },
   { path: 'customers', component: CustomersComponent },
   { path: 'reservations', component: ReservationsComponent },
+  { path: 'new-reservation', component: ReservationFormComponent},
+  { path: 'reservations/view/:id', component: ReservationFormComponent},
+  { path: 'reservations/edit/:id', component: ReservationFormComponent},
+
 ];
--- pageKeeper/src/app/services/reservations.service.ts
+++ pageKeeper/src/app/services/reservations.service.ts
@@ -1,6 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { observable } from './../../../node_modules/rxjs/src/internal/symbol/observable';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Reservation } from '../reservations/reservation';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +16,15 @@ export class ReservationsService {
 
   getReservations(): Observable<any> {
     return this.http.get(this.endpoint)
+  }
+
+  addReservation(reservation: Reservation): Observable<any> {
+    const headers = new HttpHeaders({'Content-Type': 'application/json'})
+    return this.http.post(this.endpoint, reservation, {headers })
+  }
+
+  completeReservation(id: string): Observable<any>{
+    const url = `${this.endpoint}/${id}/complete`;
+    return this.http.post(url, {})
   }
 }
